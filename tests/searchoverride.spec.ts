622aa91d19540923e6c4b8f7f77f853b86cb6b12
Maxim Voistrik <maxim.voistrik@kodaris.com>
2025-08-24 23:56:59 +0300

    fixed search tests

---
+++ tests/searchoverride.spec.ts
@@ -0,0 +1,38 @@
+// @ts-check
+
+import { test, expect } from '@playwright/test';
+import { UIReference, outcomeMarker, inputValues, slugs } from '@config';
+
+import SearchPage from '@poms/frontend/override/search.page';
+
+test.describe('Search functionality', () => {
+  test('Search_query_returns_multiple_results', async ({ page }) => {
+    await page.goto('');
+    const searchPage = new SearchPage(page);
+    await searchPage.search(inputValues.search.queryMultipleResults);
+    await expect(page).toHaveURL(/catalogsearch\/result/);
+    const results = page.locator('.ds-sdk-product-item');
+    const resultCount = await results.count();
+    expect(resultCount).toBeGreaterThan(1);
+  });
+
+  test('User_can_find_a_specific_product_and_navigate_to_its_page', async ({ page }) => {
+    await page.goto('');
+    const searchPage = new SearchPage(page);
+    await searchPage.search(inputValues.search.querySpecificProduct);
+    const productLink = page.locator('.ds-sdk-product-item__product-name')
+        .filter({ hasText: inputValues.search.querySpecificProduct })
+        .first();
+
+    await expect(productLink, 'Product should be found in search results').toBeVisible();
+    await productLink.click()
+    await expect(page).toHaveURL(/darling-la-dolce-vita-face-body-suncare-kit/);
+  });
+
+  test('No_results_message_is_shown_for_unknown_query', async ({ page }) => {
+    await page.goto('');
+    const searchPage = new SearchPage(page);
+    await searchPage.search(inputValues.search.queryNoResults);
+    await expect(page.getByText(outcomeMarker.search.noResultsMessage)).toBeVisible();
+  });
+});
